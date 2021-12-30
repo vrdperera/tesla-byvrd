27f9172a1849974512ea86d67a22d407e0327da6
Viranga Perera <vrdperera@gmail.com>
2021-12-31 04:54:53 +0530

    Setup the project for tesla clone

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>Hiii</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
